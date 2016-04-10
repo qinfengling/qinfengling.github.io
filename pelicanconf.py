98fe86a76bf21df1a4ef21780a22ed5fbfefc1e1
Mikeqin <Fengling.Qin@gmail.com>
2016-04-10 17:15:45 +0800

    Typo

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'qinfengling'
 AUTHOR_EMAIL = u'Fengling.Qin@gmail.com'
-SITENAME = u'qinfenging.github.io'
+SITENAME = u'qinfengling.github.io'
 SITEURL = ''
 
 GITHUB_USER = u'qinfengling'
