8791199e8f29da527e2d469cc0d8f2c022fb4317
qinfengling <Fengling.Qin@gmail.com>
2019-03-07 10:57:24 +0800

    Add SSL support

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://qinfengling.pro'
+SITEURL = 'https://qinfengling.pro'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = ''
